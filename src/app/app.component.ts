eb4ed330653a82a1d83a633b2339ce9a156b319c
Gayathriamirtharaj <gayathriamirtharaj@gmail.com>
2020-07-03 02:41:04 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'price-list';
+
+
+  
+  title = 0;
+
+
+ dataList=[
+  {feature:"Single user",id:1},{feature:"5GB Storage",id:2},{feature:"Unlimited Public Projects",id:3},{feature:"Community Access",id:4},
+  {feature:"Unlimited Private Projects",id:5},{feature:"Dedicated Phone Support",id:6},{feature:"Free Subdomain",id:7}
+  ,{feature:"Monthly Status Reports",id:8}
+]
+ dataList1=[
+  {feature:"5 Users",id:1},
+  {feature:"50GB Storage",id:2},
+  {feature:"Unlimited Public Project",id:3},
+  {feature:"Community Access",id:4},
+  {feature: "Unlimited Private Projects",id:5},
+  {feature:"Dedicated Phone Support",id:6},{feature:"Free Subdomain",id:7},{feature:
+  "Monthly Status Reports",id:8}
+]
+ dataList2=[
+  {feature:"Unlimited Users",id:1},{feature:"150GB Storage",id:2},{feature:"Unlimited Public Projects",id:3},{feature:
+  "Community Access",id:4},{feature:"Unlimited Private Projects",id:5},{feature:"Dedicated Phone Support",id:6},
+  {feature:"Unlimited Free Subdomains",id:7},{feature:"Monthly Status Reports",id:8}
+]
+
+
+
 }
